46fae5b25e738db103126ea160b7176dc3fc2dae
OnaNixon <65618257+OnaNixon@users.noreply.github.com>
2021-05-24 12:14:37 -0700

    vb desktop app

---
+++ Form1.Designer.vb
@@ -0,0 +1,189 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Delete = New System.Windows.Forms.Button()
+        Me.Update = New System.Windows.Forms.Button()
+        Me.Save = New System.Windows.Forms.Button()
+        Me.Display = New System.Windows.Forms.DataGridView()
+        Me.TextId = New System.Windows.Forms.TextBox()
+        Me.TextName = New System.Windows.Forms.TextBox()
+        Me.TextPhone = New System.Windows.Forms.TextBox()
+        CType(Me.Display, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(84, 93)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(25, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Id"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(54, 191)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Gainsboro
+        Me.Label3.Location = New System.Drawing.Point(230, 29)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(310, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "CONTACTS  MANAGEMENT"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.Label4.Location = New System.Drawing.Point(49, 281)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(60, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Phone"
+        '
+        'Delete
+        '
+        Me.Delete.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Delete.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Delete.ForeColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.Delete.Location = New System.Drawing.Point(342, 359)
+        Me.Delete.Name = "Delete"
+        Me.Delete.Size = New System.Drawing.Size(75, 23)
+        Me.Delete.TabIndex = 7
+        Me.Delete.Text = "Delete"
+        Me.Delete.UseVisualStyleBackColor = False
+        '
+        'Update
+        '
+        Me.Update.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Update.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Update.ForeColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.Update.Location = New System.Drawing.Point(205, 359)
+        Me.Update.Name = "Update"
+        Me.Update.Size = New System.Drawing.Size(75, 23)
+        Me.Update.TabIndex = 8
+        Me.Update.Text = "Update"
+        Me.Update.UseVisualStyleBackColor = False
+        '
+        'Save
+        '
+        Me.Save.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Save.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Save.ForeColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.Save.Location = New System.Drawing.Point(57, 359)
+        Me.Save.Name = "Save"
+        Me.Save.Size = New System.Drawing.Size(75, 23)
+        Me.Save.TabIndex = 9
+        Me.Save.Text = "Save"
+        Me.Save.UseVisualStyleBackColor = False
+        '
+        'Display
+        '
+        Me.Display.BackgroundColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Display.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.Display.GridColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Display.Location = New System.Drawing.Point(433, 93)
+        Me.Display.Name = "Display"
+        Me.Display.Size = New System.Drawing.Size(355, 289)
+        Me.Display.TabIndex = 10
+        '
+        'TextId
+        '
+        Me.TextId.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.TextId.Location = New System.Drawing.Point(133, 92)
+        Me.TextId.Name = "TextId"
+        Me.TextId.Size = New System.Drawing.Size(284, 20)
+        Me.TextId.TabIndex = 11
+        '
+        'TextName
+        '
+        Me.TextName.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.TextName.Location = New System.Drawing.Point(133, 190)
+        Me.TextName.Name = "TextName"
+        Me.TextName.Size = New System.Drawing.Size(284, 20)
+        Me.TextName.TabIndex = 12
+        '
+        'TextPhone
+        '
+        Me.TextPhone.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.TextPhone.Location = New System.Drawing.Point(133, 280)
+        Me.TextPhone.Name = "TextPhone"
+        Me.TextPhone.Size = New System.Drawing.Size(284, 20)
+        Me.TextPhone.TabIndex = 13
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Maroon
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextPhone)
+        Me.Controls.Add(Me.TextName)
+        Me.Controls.Add(Me.TextId)
+        Me.Controls.Add(Me.Display)
+        Me.Controls.Add(Me.Save)
+        Me.Controls.Add(Me.Update)
+        Me.Controls.Add(Me.Delete)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "project"
+        CType(Me.Display, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Delete As Button
+    Friend WithEvents Update As Button
+    Friend WithEvents Save As Button
+    Friend WithEvents Display As DataGridView
+    Friend WithEvents TextId As TextBox
+    Friend WithEvents TextName As TextBox
+    Friend WithEvents TextPhone As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Dim con As New SqlConnection("Data Source=DESKTOP-R6H7V2T\SQLEXPRESS;Initial Catalog=kowero;Integrated Security=True")
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadDataInGrid()
+    End Sub
+
+    Private Sub LoadDataInGrid()
+        con.Open()
+        Dim command As New SqlCommand("Select * from contacts", con)
+        Dim sda As New SqlDataAdapter(command)
+        Dim dt As New DataTable
+        sda.Fill(dt)
+        Display.DataSource = dt
+        con.Close()
+    End Sub
+
+    Private Sub Save_Click(sender As Object, e As EventArgs) Handles Save.Click
+        Dim pid As Integer = Convert.ToInt32(TextId.Text)
+        Dim name As String = TextName.Text
+        Dim number As String = TextPhone.Text
+        con.Open()
+        Dim command As New SqlCommand("Insert into contacts(id,name,number) values ('" & pid & "','" & name & "','" & number & "')", con)
+        command.ExecuteNonQuery()
+        con.Close()
+        MessageBox.Show("Successfully Inserted")
+        LoadDataInGrid()
+    End Sub
+
+
+
+    Private Sub Update_Click(sender As Object, e As EventArgs) Handles Update.Click
+        Dim pid As Integer = Convert.ToInt32(TextId.Text)
+        Dim name As String = TextName.Text
+        Dim number As String = TextPhone.Text
+        con.Open()
+        Dim command As New SqlCommand("update contacts set name = '" & name & "', number = '" & number & "' where id = '" & pid & "'", con)
+        command.ExecuteNonQuery()
+        con.Close()
+        MessageBox.Show("Successfully updated")
+        LoadDataInGrid()
+    End Sub
+
+    Private Sub Delete_Click(sender As Object, e As EventArgs) Handles Delete.Click
+        Dim pid As Integer = Convert.ToInt32(TextId.Text)
+        con.Open()
+        Dim command As New SqlCommand("delete contacts where id = '" & pid & "'", con)
+        command.ExecuteNonQuery()
+        con.Close()
+        MessageBox.Show("Successfully deleted.")
+        LoadDataInGrid()
+    End Sub
+End Class
